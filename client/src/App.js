86d9c68aa5b74fe7c26eb66afc83d0fed73d84ab
Rooster <othmanmuhidean@gmail.com>
2020-04-14 21:01:14 -0700

    components added

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Autocomplete from "./components/autocomplete/autocomplete";
+
+function App() {
+  return (
+    <div>
+      <Autocomplete />
+    </div>
+  );
+}
+
+export default App;
